20b4373bc0ba4f81ac44ea0bae61cb0c94e6181c
Dylan <lopezdylanca@gmail.com>
2022-05-07 11:38:44 -0400

    Fixed syntax errer and added styling changes

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,12 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +  // Syntax error fix : Added +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}} <span>({{file_size}} {{file_type}})</span></a>' + // Adding in span to add file name and type                  
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
